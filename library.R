344b16fb62ac9cfa5968f6d8bc9eb55d02f1f12f
bgulbis <brian.gulbis@gmail.com>
2016-02-16 09:51:20 -0600

    initial commit

---
+++ library.R
@@ -0,0 +1,12 @@
+# library.R
+# 
+# packages needed for data tidying and analysis
+# 
+
+library(BGTools)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(stringr)
+library(ggplot2)
+library(ReporteRs)
